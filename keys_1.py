818949a7e5ca09f480a4cdbaea15a204106c8bd7
Mikhail_Z <m.zimnev@yandex.ru>
2022-12-15 17:22:40 +0300

    Version 15.12.2022 Graphics

--- keys_1.py
+++ keys_1.py
@@ -1,9 +1,64 @@
 ''' Counting key skills from vacancy requests and summarise them'''
 
 import matplotlib.pyplot as plt
-
+d = {}
+top20 = {}
 def stat():
     ''' To count statitic of skills frequency '''
+
+    ss = []
+    with open('key_skills.txt', encoding = 'utf-8') as fi:
+        #d = {}
+        total, hm = 0, 0
+        for s in fi:
+            if s.strip() == '': continue
+            hm += 1
+            if s in ss: continue
+            ss.append(s)
+            total += 1
+            for ii in s.split(','):
+                i = ii.strip().lower()
+                if i == 'алгоритмы':
+                    i = 'алгоритмы и структуры данных'
+                    ii = 'Алгоритмы и структуры данных'
+                elif i == 'django':
+                    i = 'django framework'
+                    ii = 'Django Framework'
+                elif i == 'airflow':
+                    i = 'Apache Airflow'.lower()
+                    ii = 'Apache Airflow'
+                elif i == 'английский язык':
+                    i = 'английский'
+                    ii = 'Английский'
+                elif i == 'c' or i == 'c++' or i == 'c/c++':
+                    i = 'c/c++'
+                    ii = 'C/C++'
+                elif i == 'ml' or i == 'machine learning':
+                    i == 'ml'
+                    ii = 'Machine Learning'
+                elif i == 'js' or i == 'javascript':
+                    i == 'js'
+                    ii = 'Javascript'
+
+                elif i == ' ' or i == '':
+                    continue
+
+                if i not in ['python']:
+                    if i not in d:
+                        d[i] = [0, ii.strip()]
+                    d[i][0] = d[i][0] + 1
+        #top20 = {}
+        for i in sorted(d, key=lambda x: (-d[x][0], d[x][1]))[:20]:
+            top20[d[i][1]] = d[i][0]
+        return hm, total
+        #for j, i in enumerate(sorted(d, key=lambda x: (-d[x][0], d[x][1])), 1):
+        #    print(f"{j:>3} {d[i][1]:<28} {d[i][0]:>3} {round(d[i][0] / total, 4):6.2%}")
+        #    if j % 10 == 0:
+        #        pass
+        #        input("-------------")
+
+def stat_file():
+    ''' To count statitic of skills frequency and to write results to file'''
 
     ss = []
     with open('key_skills.txt', encoding = 'utf-8') as fi, open('result_keys.txt', 'w', encoding = 'utf-8') as fo:
@@ -47,41 +102,47 @@ def stat():
                         d[i] = [0, ii.strip()]
                     d[i][0] = d[i][0] + 1
 
-        top20 = {}
-        for i in sorted(d, key=lambda x: (-d[x][0], d[x][1]))[:20]:
-            top20[d[i][1]] = d[i][0]
+        #top20 = {}
+        #for i in sorted(d, key=lambda x: (-d[x][0], d[x][1]))[:20]:
+        #    top20[d[i][1]] = d[i][0]
 
-        print(f"Всего вакансий = {hm}, из них уникальных = {total}")
+def stat_to_screen():
+    ''' function of displaing result to screen'''
 
-        for j, i in enumerate(sorted(d, key=lambda x: (-d[x][0], d[x][1])), 1):
-            print(f"{j:>3} {d[i][1]:<28} {d[i][0]:>3} {round(d[i][0] / total, 4):6.2%}")
-            print(f"{j:>3} {d[i][1]:<28} {d[i][0]:>3} {round(d[i][0] / total, 4):6.2%}", file=fo)
-            if j % 10 == 0:
-                pass
-                input("-------------")
-
-
+    hm, total = stat()
+    print(f"Всего вакансий = {hm}, из них уникальных = {total}")
+    input("-------------")
+    print('Start to write')
+    for j, i in enumerate(sorted(d, key=lambda x: (-d[x][0], d[x][1])), 1):
+        print(f"{j:>3} {d[i][1]:<28} {d[i][0]:>3} {round(d[i][0] / total, 4):6.2%}")
+        if j % 10 == 0:
+            input("-------------")
+    print("-------------")
+    return
 def plot_matplotlib():
     ''' Graphic chat on Top20  (matplotlib version)'''
-    import matplotlib as plt
+
+    import matplotlib.pyplot as plt
     import pandas as pd
-    # d = {'SQL': 24, 'PostgreSQL': 18, 'Git': 15, 'Linux': 13, 'Английский': 10, 'docker': 9, 'Django Framework': 8, 'MySQL': 6, 'ClickHouse': 5, 'Kubernetes': 5, 'RabbitMQ': 5, 'Tableau': 5, 'flask': 5, 'Apache Airflow': 4, 'Redis': 4, 'ООП': 4, 'C/C++': 3, 'DWH': 3, 'Data Mining': 3, 'Golang': 3}
+    #top20 = {'SQL': 24, 'PostgreSQL': 18, 'Git': 15, 'Linux': 13, 'Английский': 10, 'docker': 9, 'Django Framework': 8, 'MySQL': 6, 'ClickHouse': 5, 'Kubernetes': 5, 'RabbitMQ': 5, 'Tableau': 5, 'flask': 5, 'Apache Airflow': 4, 'Redis': 4, 'ООП': 4, 'C/C++': 3, 'DWH': 3, 'Data Mining': 3, 'Golang': 3}
     dind = sorted(top20.keys(), key=lambda x: (top20[x], x))
     df = pd.Series(top20, index=dind)
-    print(df)
+    #print(df)
     df.plot(kind='barh')
+    plt.show()
+    input('more?')
 
 
 def plot_seaborn():
     ''' Graphic chat on Top20  (seaborn version)'''
     import matplotlib.pyplot as plt
     import seaborn as sns
-    d = {'SQL': 24, 'PostgreSQL': 18, 'Git': 15, 'Linux': 13, 'Английский': 10, 'docker': 9, 'Django Framework': 8,
-         'MySQL': 6, 'ClickHouse': 5, 'Kubernetes': 5, 'RabbitMQ': 5, 'Tableau': 5, 'flask': 5, 'Apache Airflow': 4,
-         'Redis': 4, 'ООП': 4, 'C/C++': 3, 'DWH': 3, 'Data Mining': 3, 'Golang': 3}
 
-    y = list(d.values())
-    x = list(d.keys())
+    y = list(top20.values())
+    x = list(top20.keys())
 
     sns.barplot(x=y, y=x)
+    plt.show()
+    input('more?')
+
 
--- main.py
+++ main.py
@@ -4,4 +4,21 @@
 import keys_1
 
 if __name__ == '__main__':
-    keys_1.stat()
+    while True:
+        print("1. Статистика")
+        print("2. Matplotlib")
+        print("3. Seaborn")
+        print("4. Статистика в файл")
+        print("0. Выход")
+        x = 0
+        try:
+            x = int(input('==>'))
+        except:
+            x = 0
+
+        match x:
+            case 1: keys_1.stat_to_screen()
+            case 2: keys_1.plot_matplotlib()
+            case 3: keys_1.plot_seaborn()
+            case 4: keys_1.stat_file()
+            case 0: break
